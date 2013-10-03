4ae70297538f73ccffac470d1054bb11775c8bcf
Marek Otahal <markotahal@gmail.com>
2013-10-04 01:45:44 +0200

    ROS use dummy publisher
    function publisher() is just a dummy pass
    if the fucntionality is really needed, NTA_ROS_PUB is set and we assign
    _publisher() to it, that does the work.
    
    Usecase: in classes, we dont need to check if NTA_ROS_PUB is on/off,
    just call publish() and it will behave smart :)

--- lang/ros/ros.py
+++ lang/ros/ros.py
@@ -13,18 +13,25 @@ def ros_init(pub_topic):
 #####################################
 def _init(topic):
   import rospy                        # ros python support
-  from std_msgs.msg import String     # import from the ROS standard msg set (*.msg) a msg of type String
+  from std_msgs.msg import String     # import from the ROS standard msg set (*.msg) a msg of type String #TODO: use numpy array
   try:
     if os.environ["NTA_ROS_PUB"]:     # Publisher functionality
       pub = rospy.Publisher(topic, String)    # create new publisher which will publish to the topic
       rospy.init_node('talker', anonymous=True)   # init new ROS node called 'talker'
-  
+      publish = _publish  # hack: if NTA_ROS_PUB is set, assign a _publish fn that actually does something, otherwise just dummy skip
   except KeyError:
     pass
 
 
 #####################################
-def publish(str):
+def _publish(str):
+  """the publisher functionality that publishes string str on topic (given in ros_init)"""
   if not rospy.is_shutdown():               # check for ROS node to be killed
     rospy.loginfo(str)                      # print to ROS console
     pub.publish(str)                        # publish the message
+
+
+#####################################
+def publish(str):
+  """a dummy fn that just skips"""
+  pass
