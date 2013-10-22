e8888cb4e22421d6a4e1d87d54130f2847abdf82
Marek Otahal <markotahal@gmail.com>
2013-10-23 01:50:13 +0200

    add scn-build -k option

--- build.sh
+++ build.sh
@@ -89,12 +89,12 @@ function pythonSetup {
 }
 
 function doConfigure {
-  scan-build -analyze-headers -enable-checker alpha "$NUPIC/configure" --enable-optimization --enable-assertions=yes --prefix="$NUPIC_INSTALL"
+  scan-build -k -analyze-headers -enable-checker alpha "$NUPIC/configure" --enable-optimization --enable-assertions=yes --prefix="$NUPIC_INSTALL"
   exitOnError $?
 }
 
 function doMake {
-  scan-build -analyze-headers -enable-checker alpha make -j $MK_JOBS -o $BUILDDIR
+  scan-build -k -analyze-headers -enable-checker alpha make -j $MK_JOBS -o $BUILDDIR
   make install
   exitOnError $?
 }
