b2f411b4bdde9c89fefdfc5d124b7d68b4aab1b2
Marek Otahal <markotahal@gmail.com>
2013-11-23 10:22:41 +0100

    ROSEncoder: msgFormat param is of type std_msg,

--- py/nupic/encoders/extras/ros.py
+++ py/nupic/encoders/extras/ros.py
@@ -4,17 +4,17 @@ from nupic.encoders.passthru import PassThruEncoder as I
 class ROSEncoder(I):
   """ ROS """
 
-  def __init__(self, n, topic, nodeType, format="String", name="ROS"):
+  def __init__(self, n, topic, nodeType, msgFormat, name="ROS"):
     """params:
          n -- #bits of input (== also #bits of output);
          topic -- a "string" for Publisher, a list of strings for Listener;
 	 nodeType -- type of ROS node - values: "Publisher", "Listener"
-         format -- (string) type of the ROS msg, see http://wiki.ros.org/std_msgs
+         msgFormat -- (obj) type of the ROS msg, see http://wiki.ros.org/std_msgs
     """
     super(ROSEncoder, self).__init__(n, w=None, multiply=1, name=name, forced=True)
     self.type = nodeType
     self.topic = topic
-    self.format = format
+    self.format = msgFormat
 
     if nodeType not in ["Listener", "Publisher"]:
       raise Exception("ROS: nodeType must be one of \"Listener\", \"Publisher\" ")
