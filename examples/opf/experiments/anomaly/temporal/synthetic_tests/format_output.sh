b6f4ea2a402786ce4d2bb131cf74d0df8fb96a82
Marek Otahal <markotahal@gmail.com>
2015-09-17 13:20:30 +0200

    matlab script also plot and load results

---
+++ examples/opf/experiments/anomaly/temporal/synthetic_tests/format_output.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# formats output from CLA's inference
+# ./formatOutput.sh inferenceResultsFile.csv
+# 2 = actual/raw value
+# 7 = anomaly score
+
+#1 get rid of the {12 232 232} field that breaks "normal" CSV 
+# cut only the ecg (orig value) and pred.1 (prediction for T+1)
+cat "$1" | sed -e's/{.*}/X/g' | cut -d, -f7 
+
