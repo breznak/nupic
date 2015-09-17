c71ca6e7454884abc63232f915cfc9972dd356be
Marek Otahal <markotahal@gmail.com>
2015-09-17 13:57:57 +0200

    add results for anomaly on Random noise (10000)

--- examples/opf/experiments/anomaly/temporal/synthetic_tests/format_output.sh
+++ examples/opf/experiments/anomaly/temporal/synthetic_tests/format_output.sh
@@ -6,5 +6,5 @@
 
 #1 get rid of the {12 232 232} field that breaks "normal" CSV 
 # cut only the ecg (orig value) and pred.1 (prediction for T+1)
-cat "$1" | sed -e's/{.*}/X/g' | cut -d, -f7 
+cat "$1" | sed -e's/{.*}/X/g' | sed -e's/\[.*\]/Y/g' | cut -d',' -f7 
 
